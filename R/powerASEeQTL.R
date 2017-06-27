54ac4a6adf14d4746bc84e3bfed7184a3138b09d
athwing <athwing@yahoo.com>
2017-06-26 17:09:17 -0700

    Updated man pages, legend, order of methods, color

--- R/powerASEeQTL.R
+++ R/powerASEeQTL.R
@@ -1,15 +1,17 @@
 
 powerASEeQTL <-
-function(n,mu=500, n.simu=200, methods=c("Linear Model", "Negative Binomial", "TReC", "TReCASE", "ASEchi2","ASEbinom","Poisson"), legend=TRUE, color=TRUE, folds= seq(1.5, 1.7, by=0.2), alpha=0.001, phi=1.0, theta=0.1, maf=0.2, numcol=1, title="", subtitle="", titlecolor="black", subtitlecolor="black", titlesize=1, subtitlesize=1, legendbox="o", labelsize = 1, labelcolor = "black", linewidth=2, tilt=0)
+function(n,mu=500, n.simu=200, methods=c("linear", "negativeB", "poisson","TReC","ASEchi2","ASEbinom", "TReCASE"), legend=TRUE, color=TRUE, folds= seq(1.5, 1.7, by=0.2), alpha=0.001, phi=1.0, theta=0.1, maf=0.2, numcol=1, title="", subtitle="", titlecolor="black", subtitlecolor="black", titlesize=1, subtitlesize=1, legendbox="o", labelsize = 1, labelcolor = "black", linewidth=2, tilt=0)
 {
   
   library(VGAM)
   library(MASS)
 
-possibleMethods <- c("Linear Model", "Negative Binomial", "TReC", "TReCASE", "ASEchi2", "ASEbinom", "Poisson")
+  # Used to be: possibleMethods <- c("Linear", "Negative Binomial", "TReC", "TReCASE", "ASEchi2", "ASEbinom", "Poisson")
+possibleMethods <- c("linear", "negativeB", "poisson","TReC","ASEchi2","ASEbinom", "TReCASE")
 
 numMethods <- length(possibleMethods)
 
+colorVec <- c("black","red","green3","blue","cyan","magenta","mediumorchid4")
 
 #CHECKS
 #Check that the length of folds is > 1
@@ -23,7 +25,7 @@ for(i in 1:length(methods)){
  
 #Allow for grayscale by creating color vector based on color=TRUE/FALSE
 if(color){
-  colVec<- c(1:numMethods)
+  colVec<- colorVec
 }else{
   colVec <- gray.colors(numMethods)
 }
@@ -38,8 +40,8 @@ ltyv <- c(1:numMethods)
 
 matResults<-matrix(0,nrow=length(folds),ncol=numMethods)
 
-
-colnames(matResults)<-c("Linear","glmNB","TReC","TReCASE","ASEchi2","ASEbinom","Poisson")
+# Order from powerRNAseq: (linear, negbin, TReC, TReCASE, ASEchi2, ASEbinom, poisson)
+colnames(matResults)<-c("linear","negativeB","TReC","TReCASE","ASEchi2","ASEbinom","poisson")
 
 for(i in 1:length(folds)){
 print(paste("folds",i,"in",length(folds)))
@@ -67,20 +69,26 @@ plot(-1,-1,xlim=c(min(folds),max(folds)),ylim=c(0,1),main=title,sub=subtitle,col
 
 
 for(mi in 1:length(methods)){
-    if(methods[mi]=="Linear Model"){lines(folds,matResults[,1],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
-    if(methods[mi]=="Negative Binomial"){lines(folds,matResults[,2],pchv[mi],colVec[mi],type="b",ltyv[mi], lwd=linewidth)}
+    if(methods[mi]=="linear"){lines(folds,matResults[,1],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
+    if(methods[mi]=="negativeB"){lines(folds,matResults[,2],pchv[mi],colVec[mi],type="b",ltyv[mi], lwd=linewidth)}
+  if(methods[mi]=="poisson"){lines(folds,matResults[,7],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
     if(methods[mi]=="TReC"){lines(folds,matResults[,3],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
+  if(methods[mi]=="ASEchi2"){lines(folds,matResults[,5],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
+  if(methods[mi]=="ASEbinom"){lines(folds,matResults[,6],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
     if(methods[mi]=="TReCASE"){lines(folds,matResults[,4],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
-    if(methods[mi]=="ASEchi2"){lines(folds,matResults[,5],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
-    if(methods[mi]=="ASEbinom"){lines(folds,matResults[,6],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
-    if(methods[mi]=="Poisson"){lines(folds,matResults[,7],pchv[mi],colVec[mi],type="b",ltyv[mi],lwd=linewidth)}
+
+    
 }
 
-if(legend){legend(min(folds),1,methods,fill=colVec,col=colVec,lty=ltyv,pch=pchv,ncol=numcol,bty=legendbox)}
+leg.text <- c("eQTL: Linear Regression","eQTL: Negative binomial","eQTL: Poisson Regression","eQTL: TreC","ASE: chi-square",
+              "ASE: binomial test","eQTL & ASE: TreCASE")
+
+if(legend){legend(min(folds),1,legend = leg.text,fill=colVec,col=colVec,lty=ltyv,pch=pchv,ncol=numcol,bty=legendbox)}
 
 }
 
 dev.off()
 
-
+# Return matResults
+matResults
 }
